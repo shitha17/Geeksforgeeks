7b4199469ea16dc26cc9175722a29f5a89b3273b
Varshitha Reddy <45087191+shitha17@users.noreply.github.com>
2024-04-18 16:55:07 -0500

    Added Solution - GfG to GitHub

---
+++ Medium/Find missing in second array/find-missing-in-second-array.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+
+class Solution:
+    def findMissing(self,a,b,n,m):
+    # code here
+   
+        
+        set_b = set(b)
+        ans = []
+        
+        for i in a:
+            if i not in set_b:
+                ans.append(i)
+                
+        
+        return ans
+            
+         
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+t=int(input())
+for _ in range(0,t):
+   # n=int(input())
+    l = list(map(int, input().split()))
+    n=l[0]
+    m=l[1]
+    a = list(map(int,input().split()))
+    b = list(map(int, input().split()))
+    ob=Solution()
+    ans=ob.findMissing(a,b,n,m)
+    for each in ans:
+        print(each,end=' ')
+    print()
+# } Driver Code Ends
